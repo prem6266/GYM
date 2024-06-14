b1e55d490d08e50b20400c40dc8224abe55575eb
unknown <premjoshi6266@gmail.com>
2024-06-14 23:09:00 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,46 @@
-import Navbar from "@/scences/navbar"
-import Home from "@/scences/home";
-import Benefits from "@/scences/benefits";
-import OurClasses from "@/scences/ourClasses";
-import ContactUs from "@/scences/contactUs";
-import Footer from "@/scences/footer";
-import { useEffect, useState } from "react"
+import Navbar from "./scences/navbar";
+import Home from "./scences/home";
+import OurClasses from "./scences/ourClasses";
+import ContactUs from "./scences/contactUs";
+import Footer from "./scences/footer";
+import Benefits from "./scences/benefits";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
-enum SelectedPage{
-  Home = "home",
-  Benefits = "benefits",
-  OurClasses = "ourclasses",
-  ContactUs = "contactus",
 
-}
+
+
 function App() {
-const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
-const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
-useEffect(() => {
-  const handleScroll = () => {
-    if (window.scrollY === 0) {
-      setIsTopOfPage(true);
-      setSelectedPage(SelectedPage.Home);
-    }
-    if (window.scrollY !== 0) setIsTopOfPage(false);
-  };
-  window.addEventListener("scroll", handleScroll);
-  return () => window.removeEventListener("scroll", handleScroll);
-}, []);
-
-
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className="app bg-gray-20 w-56">
-      <Navbar 
-     isTopOfPage={isTopOfPage}
-     selectedPage={selectedPage}
-     setSelectedPage={setSelectedPage}/>
-     <Home setSelectedPage={setSelectedPage}/>
-     <Benefits setSelectedPage={setSelectedPage}/>
-     <OurClasses setSelectedPage={setSelectedPage}/>
-     <ContactUs setSelectedPage={setSelectedPage}/>
-     <Footer />
+    <div className="app bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
